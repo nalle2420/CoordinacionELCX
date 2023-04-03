0e46441f545e90376b514b9731db3d9ef67eeab3
Nallely Reyes <na_reyesd@unicah.edu>
2023-04-02 21:19:08 -0600

    Paneles para disenio

--- Coordinacion/Form1.Designer.cs
+++ Coordinacion/Form1.Designer.cs
@@ -28,19 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            BarraTitulo = new FlowLayoutPanel();
+            BarraLateral = new Panel();
+            PanelControlador = new Panel();
+            btnCerrar = new PictureBox();
+            BarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
             SuspendLayout();
+            // 
+            // BarraTitulo
+            // 
+            BarraTitulo.BackColor = Color.FromArgb(56, 9, 120);
+            BarraTitulo.Controls.Add(btnCerrar);
+            BarraTitulo.FlowDirection = FlowDirection.RightToLeft;
+            BarraTitulo.Location = new Point(0, 0);
+            BarraTitulo.Name = "BarraTitulo";
+            BarraTitulo.Size = new Size(1300, 35);
+            BarraTitulo.TabIndex = 0;
+            // 
+            // BarraLateral
+            // 
+            BarraLateral.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            BarraLateral.BackColor = Color.FromArgb(26, 32, 50);
+            BarraLateral.Location = new Point(0, 35);
+            BarraLateral.Name = "BarraLateral";
+            BarraLateral.Size = new Size(250, 715);
+            BarraLateral.TabIndex = 1;
+            // 
+            // PanelControlador
+            // 
+            PanelControlador.Location = new Point(250, 35);
+            PanelControlador.Name = "PanelControlador";
+            PanelControlador.Size = new Size(1050, 715);
+            PanelControlador.TabIndex = 2;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Cursor = Cursors.Hand;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(1267, 3);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(30, 30);
+            btnCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnCerrar.TabIndex = 0;
+            btnCerrar.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1300, 750);
+            Controls.Add(PanelControlador);
+            Controls.Add(BarraLateral);
+            Controls.Add(BarraTitulo);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            BarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel BarraTitulo;
+        private PictureBox btnCerrar;
+        private Panel BarraLateral;
+        private Panel PanelControlador;
     }
 }
