3d3e8fa70f31254246538917f8f62128ca10bd49
Nallely Reyes <na_reyesd@unicah.edu>
2023-04-06 16:36:03 -0600

    Diseño completo

--- Coordinacion/Coordinación.Designer.cs
+++ Coordinacion/Coordinación.Designer.cs
@@ -34,7 +34,22 @@
             btnAgregar = new Button();
             button1 = new Button();
             button2 = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            panel1 = new Panel();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -51,7 +66,8 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(38, 236);
+            dataGridView1.Location = new Point(38, 247);
+            dataGridView1.Margin = new Padding(0);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 25;
@@ -70,7 +86,7 @@
             btnAgregar.Location = new Point(38, 62);
             btnAgregar.Margin = new Padding(0);
             btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(237, 37);
+            btnAgregar.Size = new Size(207, 37);
             btnAgregar.TabIndex = 2;
             btnAgregar.Text = "Agregar";
             btnAgregar.UseVisualStyleBackColor = false;
@@ -84,10 +100,10 @@
             button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleRight;
-            button1.Location = new Point(343, 62);
+            button1.Location = new Point(277, 62);
             button1.Margin = new Padding(0);
             button1.Name = "button1";
-            button1.Size = new Size(237, 37);
+            button1.Size = new Size(207, 37);
             button1.TabIndex = 3;
             button1.Text = "Modificar";
             button1.UseVisualStyleBackColor = false;
@@ -101,19 +117,152 @@
             button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             button2.Image = (Image)resources.GetObject("button2.Image");
             button2.ImageAlign = ContentAlignment.MiddleRight;
-            button2.Location = new Point(642, 62);
+            button2.Location = new Point(519, 62);
             button2.Margin = new Padding(0);
             button2.Name = "button2";
-            button2.Size = new Size(237, 37);
+            button2.Size = new Size(207, 37);
             button2.TabIndex = 4;
             button2.Text = "Eliminar";
             button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(780, -20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(168, 133);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(39, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 17);
+            label2.TabIndex = 6;
+            label2.Text = "Coordinador";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(344, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 17);
+            label3.TabIndex = 8;
+            label3.Text = "Técnico";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(139, 176);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(182, 23);
+            textBox3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(39, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 17);
+            label4.TabIndex = 10;
+            label4.Text = "Ticket";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(620, 126);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 17);
+            label5.TabIndex = 12;
+            label5.Text = "Serie";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(424, 178);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(376, 23);
+            textBox5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(348, 178);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 17);
+            label6.TabIndex = 14;
+            label6.Text = "Actividad";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(26, 32, 50);
+            panel1.Controls.Add(label7);
+            panel1.Location = new Point(38, 221);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(156, 26);
+            panel1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ControlLightLight;
+            label7.Location = new Point(3, 6);
+            label7.Margin = new Padding(3);
+            label7.Name = "label7";
+            label7.Size = new Size(132, 17);
+            label7.TabIndex = 17;
+            label7.Text = "Tareas pendientes";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(139, 125);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 23);
+            comboBox1.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(409, 125);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(182, 23);
+            comboBox2.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(668, 126);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(182, 23);
+            comboBox3.TabIndex = 19;
             // 
             // Coordinación
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(919, 536);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(panel1);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(btnAgregar);
@@ -123,6 +272,9 @@
             Name = "Coordinación";
             Text = "Coordinación";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -134,5 +286,18 @@
         private Button btnAgregar;
         private Button button1;
         private Button button2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private Panel panel1;
+        private Label label7;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
     }
 }
--- Coordinacion/Coordinación.cs
+++ Coordinacion/Coordinación.cs
@@ -26,5 +26,10 @@ namespace Coordinacion
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
